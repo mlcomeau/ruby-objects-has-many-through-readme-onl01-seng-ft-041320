ba3a1187ff62c5abbe60c60b792f0e53d1f7b517
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 17:56:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,16 +1,16 @@
 class Waiter
   class Waiter
- 
+
   attr_accessor :name, :yrs_experience
- 
+
   @@all = []
- 
+
   def initialize(name, yrs_experience)
     @name = name
     @yrs_experience = yrs_experience
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
